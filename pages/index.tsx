ac67e5598168cda23d314b87a3acbda3cf617a66
Razvan <rabe@dhigroup.com>
2022-02-09 20:36:39 +0100

    init next app

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import React, { FC, useState } from 'react'
+import { Box } from '@mui/material'
+import { makeStyles } from '@mui/styles'
+
+const useStyles = makeStyles(theme => ({
+
+}))
+
+interface Props {
+
+}
+const index: FC<Props> = () => {
+
+	const classes = useStyles()
+
+	return (
+		<Box>
+			Razvan
+		</Box>
+	)
+
+}
+
+export default index
