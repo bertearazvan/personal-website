a04816917cad976e0ce97301330a9ce014337419
Razvan Bertea <bertearazvan>
2024-11-09 17:28:58 +0100

    add initial files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Container } from "@mui/material";
+import Hero from "./sections/Hero";
+
+function App() {
+	return (
+		<Container maxWidth="lg">
+			<Hero />
+		</Container>
+	);
+}
+
+export default App;
---
+++ src/sections/Hero.tsx
@@ -0,0 +1,11 @@
+import { Box, Stack } from "@mui/material";
+
+const Hero = () => {
+	return (
+		<Box>
+			<Stack> "test"</Stack>
+		</Box>
+	);
+};
+
+export default Hero;
